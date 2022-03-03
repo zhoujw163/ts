eff1b9ea23a3ee796e5c03bf1140a5af2380bbe9
zhoujiawei <1227364071@qq.com>
2022-03-03 15:16:38 +0800

    feat: 使用 infer 提取类型

---
+++ src/knowledge/1. 使用 infer 提取类型.ts
@@ -0,0 +1,35 @@
+// 数组元素类型提取
+type GetFirst<Arr extends unknown[]> = Arr extends [infer First, ...unknown[]] ? First : never;
+type GetFirstResult = GetFirst<[1, 2, 3]>; // 1
+
+type ShiftArr<Arr extends unknown[]> = Arr extends [unknown, ...infer Rest] ? Rest : never;
+type GetShiftArr = ShiftArr<[1, 2, 3]>; // [2, 3]
+
+// 字符串类型 StartsWith
+type StartsWith<Str extends string> = Str extends `${infer Prefix}${string}` ? Prefix : never;
+type StartsWithResult = StartsWith<'sad'>; // 's'
+
+// Trim
+type TrimRight<S extends string> = S extends `${infer Rest}${' ' | '\n' | '\t'}` ? TrimRight<Rest> : S;
+type TrimLeft<S extends string> = S extends `${' ' | '\n' | '\t'}${infer Rest}` ? TrimLeft<Rest> : S;
+type TrimResult = TrimLeft<TrimRight<'  abc  '>>; // 'abc'
+
+// 函数
+type CommonFn = (...args: any[]) => any;
+type GetParameters<T extends CommonFn> = T extends (...args: infer P) => any ? P : never;
+type GetParametersResult = GetParameters<(a: number, b: string) => void>; // [number, string]
+
+type GetThisType<T extends CommonFn> = T extends (this: infer ThisType, ...args: any) => any ? ThisType : never;
+type GetThisTypeResult = GetThisType<(this: Window, a: number, b: string) => void>; // Window
+
+// class
+type CommonConstructor = new (...args: any[]) => any;
+type Person = { name: string };
+type TestClass = {
+    new (name: string): Person
+}
+type GetConstructorParameters<T extends CommonConstructor> = T extends new (...args: infer P) => any ? P : never;
+type GetConstructorParametersResult = GetConstructorParameters<TestClass> // [name: string]
+
+
+export {}
